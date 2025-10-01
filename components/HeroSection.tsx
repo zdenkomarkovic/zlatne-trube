3f8e1dbe3dba6fe77513dbdd419e2063d3b06cfe
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-10-01 10:32:40 +0200

    .

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function HeroSection() {
   return (
-    <section className="relative h-[100vh] w-full overflow-hidden">
+    <section className="relative h-[100dvh] w-full overflow-hidden">
       <Image
         src={heroImg}
         alt="ZLATNE TRUBE hero"
